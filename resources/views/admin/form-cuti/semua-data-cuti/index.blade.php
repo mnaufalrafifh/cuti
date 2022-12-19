c6412135b2912a8178fff2062369e61863ea9405
Firman Abdi Syah Putra <firmanabdi290799@gmail.com>
2022-12-19 10:30:11 +0700

    button & notif
    button & notif

--- resources/views/admin/form-cuti/semua-data-cuti/index.blade.php
+++ resources/views/admin/form-cuti/semua-data-cuti/index.blade.php
@@ -92,19 +92,19 @@
                             @if (Auth::user()->id_roles != 3 )
                             <td>
                             <div class="btn-group" role="group" aria-label="Basic example">
-                                    <a href="{{ route('data-cuti.edit', $item->id)}}" class="btn btn-warning btn-sm btn-color btn-bg-color"  style="color: white">
-                                        <i class="bi bi-pencil-square"></i><small>Edit</small>
+                                    <a href="{{ route('data-cuti.edit', $item->id)}}" class="btn btn-warning btn-lg btn-color btn-bg-color"  style="color: white">
+                                        <i class="bi bi-pencil-square"></i>
                                     </a>
                                 <div class="btn-group"></div>
                                     <form action="{{ route('data-cuti.destroy', $item->id)}}" method="POST">
                                     @method('delete')
                                     @csrf
-                                    <button type="submit" class="btn btn-danger btn-sm btn-color btn-bg-color" onclick="return confirm('Apakah Anda Yakin Ingin Menghapus Data Ini ?')">
-                                    <i class="bi bi-trash"></i> Delete</button>
+                                    <button type="submit" class="btn btn-danger btn-lg btn-color btn-bg-color" onclick="return confirm('Apakah Anda Yakin Ingin Menghapus Data Ini ?')">
+                                    <i class="bi bi-trash"></i></button>
                                     </form>
                                 <div class="btn-group"></div>
-                                    <a href="{{ route('download.cuti', $item->id)}}" class="btn btn-primary btn-sm btn-color btn-bg-color"  style="color: white">
-                                        <i class="bi bi-download"></i><small>Download</small>
+                                    <a href="{{ route('download.cuti', $item->id)}}" class="btn btn-primary btn-lg btn-color btn-bg-color"  style="color: white">
+                                        <i class="bi bi-download"></i>
                                     </a>
                             </div>
 
